576008605c0121067865581ddaf175f35f0839b7
Han Meerholz <hanmeerholz@gmail.com>
2026-09-21 20:02:17 +0200

    Added collisions and collision handling to objects being pushed into walls.

--- Assets/Scripts/ObjectInteraction.cs
+++ Assets/Scripts/ObjectInteraction.cs
@@ -92,4 +92,45 @@ public class ObjectInteraction : MonoBehaviour
             }
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isBeingInteractedWith && interactionType == InteractionType.PUSH)
+        {
+            resolveCollision(GetComponent<Collider>().bounds, other.bounds);
+            isBeingInteractedWith = false;
+        }
+    }
+
+    private void resolveCollision(Bounds bounds, Bounds otherBounds)
+    {
+        float xOverlap = 0.0f, zOverlap = 0.0f;
+
+        if (bounds.min.x < otherBounds.min.x && otherBounds.min.x < bounds.max.x)
+        {
+            xOverlap = bounds.max.x < otherBounds.max.x ? bounds.max.x - otherBounds.min.x : otherBounds.max.x - otherBounds.min.x;
+        } else if (otherBounds.min.x < bounds.min.x && bounds.min.x < otherBounds.max.x)
+        {
+            xOverlap = otherBounds.max.x < bounds.max.x ? bounds.min.x - otherBounds.max.x : otherBounds.min.x - bounds.max.x ;
+        }
+
+        if (bounds.min.z < otherBounds.min.z && otherBounds.min.z < bounds.max.z)
+        {
+            zOverlap = bounds.max.z < otherBounds.max.z ? bounds.max.z - otherBounds.min.z : otherBounds.max.z - otherBounds.min.z;
+        }
+        else if (otherBounds.min.z < bounds.min.z && bounds.min.z < otherBounds.max.z)
+        {
+            zOverlap = otherBounds.max.z < bounds.max.z ? bounds.min.z - otherBounds.max.z : otherBounds.min.z - bounds.max.z;
+        }
+
+        if (xOverlap != 0.0f && (zOverlap == 0.0f || Mathf.Abs(xOverlap) < Mathf.Abs(zOverlap)))
+        {
+            xOverlap += (xOverlap > 0.0f) ? 0.001f : -0.001f;
+            transform.position -= new Vector3(xOverlap, 0.0f, 0.0f);
+        } else if (zOverlap != 0.0f && (xOverlap == 0.0f || Mathf.Abs(zOverlap) < Mathf.Abs(xOverlap)))
+        {
+            zOverlap += (zOverlap > 0.0f) ? 0.001f : -0.001f;
+            transform.position -= new Vector3(0.0f, 0.0f, zOverlap);
+        }
+    }
 }
