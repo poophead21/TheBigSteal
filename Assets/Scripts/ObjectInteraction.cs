446fee7703c8c60519fb0dc46db2c1e39d0d1812
Han Meerholz <hanmeerholz@gmail.com>
2026-09-21 14:23:12 +0200

    Added rotation ability.

--- Assets/Scripts/ObjectInteraction.cs
+++ Assets/Scripts/ObjectInteraction.cs
@@ -1,22 +1,30 @@
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectInteraction : MonoBehaviour
 {
     [Header("Variables")]
+    [SerializeField] private float interactDistance = 2.0f;
     [SerializeField] private float moveDistance = 2.0f;
     [SerializeField] private float moveTime = 1.0f;
-    [SerializeField] private float interactDistance = 2.0f;
+    [SerializeField] private float rotateAngle = 90.0f;
+    [SerializeField] private float rotateTime = 1.0f;
     [SerializeField] private GameObject player;
 
     private bool isBeingInteractedWith = false;
+    private enum InteractionType { PUSH, ROTATE };
+    private InteractionType interactionType = InteractionType.PUSH;
     private float moveSpeed;
+    private float rotateSpeed;
     private Vector3 moveTo;
+    private quaternion rotateTo;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveSpeed = moveDistance / moveTime;
+        rotateSpeed = rotateAngle / rotateTime;
     }
 
     // Update is called once per frame
@@ -24,34 +32,64 @@ public class ObjectInteraction : MonoBehaviour
     {
         if (isBeingInteractedWith)
         {
-            float step = moveSpeed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, moveTo, step);
-
-            if (Vector3.Distance(transform.position, moveTo) < 0.001f)
+            switch(interactionType)
             {
-                isBeingInteractedWith = false;
+                case InteractionType.PUSH:
+                    float moveStep = moveSpeed * Time.deltaTime;
+                    transform.position = Vector3.MoveTowards(transform.position, moveTo, moveStep);
+
+                    if (Vector3.Distance(transform.position, moveTo) < 0.001f)
+                    {
+                        isBeingInteractedWith = false;
+                    }
+                    break;
+                case InteractionType.ROTATE:
+                    float rotateStep = rotateSpeed * Time.deltaTime;
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateTo, rotateStep);
+
+                    if (Quaternion.Angle(transform.rotation, rotateTo) < 0.01f)
+                    {
+                        isBeingInteractedWith = false;
+                    }
+                    break;
             }
+            
 
         } else {
             Vector3 playerToObject = transform.position - player.transform.position;
 
 
+            bool pushing = player.GetComponent<PlayerMovement>().isPushing;
+            bool rotating = player.GetComponent<PlayerMovement>().isRotating;
+
+
             if (playerToObject.magnitude > interactDistance) return;
             if (Vector3.Angle(player.transform.forward, playerToObject) > 45.0f) return;
-            if (!player.GetComponent<PlayerMovement>().isInteracting) return;
+
+            if (!pushing && !rotating) return;
 
             isBeingInteractedWith = true;
 
-            Vector3 direction;
-            if (Mathf.Abs(playerToObject.x) > Mathf.Abs(playerToObject.z))
+            if (pushing)
             {
-                direction = (playerToObject.x > 0.0f) ? new Vector3(1.0f, 0.0f, 0.0f) : new Vector3(-1.0f, 0.0f, 0.0f);
-            } else
-            {
-                direction = (playerToObject.z > 0.0f) ? new Vector3(0.0f, 0.0f, 1.0f) : new Vector3(0.0f, 0.0f, -1.0f);
-            }
+                interactionType = InteractionType.PUSH;
+                Vector3 direction;
+                if (Mathf.Abs(playerToObject.x) > Mathf.Abs(playerToObject.z))
+                {
+                    direction = (playerToObject.x > 0.0f) ? new Vector3(1.0f, 0.0f, 0.0f) : new Vector3(-1.0f, 0.0f, 0.0f);
+                }
+                else
+                {
+                    direction = (playerToObject.z > 0.0f) ? new Vector3(0.0f, 0.0f, 1.0f) : new Vector3(0.0f, 0.0f, -1.0f);
+                }
 
-            moveTo = transform.position + direction * moveDistance;
+                moveTo = transform.position + direction * moveDistance;
+            } else if (rotating)
+            {
+                interactionType = InteractionType.ROTATE;
+
+                rotateTo = transform.rotation * quaternion.RotateY(rotateAngle / 180 * Mathf.PI);
+            }
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,8 @@ public class PlayerMovement : MonoBehaviour
     private Animator _animator;
     
     private bool isCrouching = false;
-    public bool isInteracting = false;
+    public bool isPushing = false;
+    public bool isRotating = false;
 
     private void Start()
     {
@@ -45,12 +46,23 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && !isCrouching)
         {
             _animator.SetTrigger("Interact");
-            isInteracting = true;
+            isPushing = true;
         }
 
         if (Input.GetKeyUp(KeyCode.E))
         {
-            isInteracting = false;
+            isPushing = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && !isCrouching)
+        {
+            _animator.SetTrigger("Interact");
+            isRotating = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            isRotating = false;
         }
     }
 
